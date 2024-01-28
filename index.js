a5ec797d5843df5945d506f26cf839252ced9ac2
mahmudul-hasan01 <mahamudul2018@gmail.com>
2024-01-28 20:18:16 +0600

    save users data

--- index.js
+++ index.js
@@ -2,12 +2,22 @@ const express = require('express')
 require('dotenv').config()
 const app = express()
 const cookieParser = require('cookie-parser')
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const jwt = require('jsonwebtoken')
 //morgan
 const cors = require('cors')
 const port = process.env.PORT || 5000
 
+const corsOptions = {
+  origin: ['http://localhost:5173'],
+  credentials: true,
+  optionSuccessStatus: 200,
+}
+app.use(cors(corsOptions))
+app.use(express.json())
+app.use(cookieParser())
+
 // Start
-const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.USER}:${process.env.PASS}@cluster0.uoehazd.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -19,17 +29,57 @@ const client = new MongoClient(uri, {
   }
 });
 
+
+const verifyToken = async (req, res, next) => {
+  const token = req.cookies?.token
+  console.log(token)
+  if (!token) {
+    return res.status(401).send({ message: 'unauthorized access' })
+  }
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      console.log(err)
+      return res.status(401).send({ message: 'unauthorized access' })
+    }
+    req.user = decoded
+    next()
+  })
+}
+
 async function run() {
   try {
-    await client.connect();
 
+    const usersCollection = client.db("StayVistaDB").collection("users");
 
     
+     app.get('/users',async(req,res) => {
+      const result = await usersCollection.find().toArray()
+      res.send(result)
+     })
+     app.put('/users/:email', async (req, res) => {
+      const email = req?.params?.email
+      const user = req?.body
+      const query = { email: email }
+      const options = { upsert: true }
+      const isExist = await usersCollection.findOne(query)
+      console.log('User found?----->', isExist)
+      if (isExist) return res.send(isExist)
+      const result = await usersCollection.updateOne(
+        query,
+        {
+          $set: { ...user, timestamp: Date.now() },
+        },
+        options
+      )
+      res.send(result)
+    })
+
+
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
